a5521e7f88b2fa2ef247f9f22bf40db6bbe2ef82
Ankita <jainankitjain@gmail.com>
2026-02-16 19:35:07 +0530

    id and server added

---
+++ src/components/Editor.js
@@ -0,0 +1,111 @@
+// import React,{useEffect} from 'react'
+// import Codemirror from 'codemirror'
+
+// import 'codemirror/lib/codemirror.css';
+// import 'codemirror/theme/dracula.css';
+// import 'codemirror/mode/javascript/javascript'
+// import 'codemirror/addon/edit/closetag';
+// import 'codemirror/addon/edit/closebrackets';
+
+// const Editor = () => {
+
+
+
+//     useEffect(()=>{
+//         async function init(){
+//             Codemirror.fromTextArea(document.getElementById('realtimeEditor'),{
+//                 mode:{name:'javascript',json:true},
+//                 theme:'dracula',
+//                 autoCloseTags:true,
+//                 autoCloseBrackets:true,
+//                 lineNumbers:true,
+//             });
+
+//         }
+//         init();
+//     },[]);
+    
+
+//   return (
+//    <textarea id="realtimeEditor"></textarea>
+//   )
+// }
+
+// export default Editor
+
+import React, { useEffect, useRef } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import ACTIONS from '../Actions';
+
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    const editorRef = useRef(null);
+    useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('realtimeEditor'),
+                {
+                    mode: { name: 'javascript', json: true },
+                    theme: 'dracula',
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
+
+            editorRef.current.on('change', (instance, changes) => {
+                const { origin } = changes;
+                const code = instance.getValue();
+                onCodeChange(code);
+                if (origin !== 'setValue') {
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
+        }
+        init();
+    }, []);
+
+    // useEffect(() => {
+    //     if (socketRef.current) {
+    //         socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+    //             if (code !== null) {
+    //                 editorRef.current.setValue(code);
+    //             }
+    //         });
+    //     }
+
+    //     return () => {
+    //         socketRef.current.off(ACTIONS.CODE_CHANGE);
+    //     };
+    // }, [socketRef.current]);
+
+    useEffect(() => {
+    if (!socketRef.current) return;
+
+    const handleCodeChange = ({ code }) => {
+        if (code !== null) {
+            editorRef.current.setValue(code);
+        }
+    };
+
+    socketRef.current.on(ACTIONS.CODE_CHANGE, handleCodeChange);
+
+    return () => {
+        if (socketRef.current) {
+            socketRef.current.off(ACTIONS.CODE_CHANGE, handleCodeChange);
+        }
+    };
+}, [socketRef]);  
+
+
+    return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,79 @@
 import React from 'react'
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
+
+    const [roomId,setRoomId]=useState('');
+    const [username,setUsername]=useState('');
+
+    const navigate=useNavigate();
+
+
+    const createNewRoom=(e)=>{
+        e.preventDefault();
+        const id =uuidv4();
+        console.log(id);
+        setRoomId(id);
+        toast.success('Created a new room');
+
+
+    }
+
+    const joinroom=()=>{
+        if(!roomId || !username){
+            toast.error('Room ID and username is required');
+            return;
+        }
+        //redirect
+        navigate(`/editor/${roomId}`,{
+            state:{
+                username,
+            }
+        })
+    }
+
+
+        const handleInputEnter=(e)=>{
+            // console.log('event',e.code);
+            if(e.code==='Enter'){
+                joinroom();
+
+            }
+
+        }
+
+
+
   return (
+    <>
     <div className="homePageWrapper">
-      <div classNAme="formWrapper">
-        <h1 classNAme ="mainLabel">Paste invitation room id</h1>
+      <div className="formWrapper">
+        <h1 className ="mainLabel">Paste invitation room id</h1>
         <div className="inputGroup">
             <input
                 type="text"
                 className="inputBox"
                 placeholder="Room ID"
+                onChange={(e)=>setRoomId(e.target.value)}
+                value={roomId}
+                onKeyUp={handleInputEnter}
             />
             <input
                 type="text"
                 className="inputBox"
                 placeholder="Username"
+                onChange={(e)=>setUsername(e.target.value)}
+                value={username}
+                onKeyUp={handleInputEnter}
             />
-            <button className ="btn joinBtn">Join</button>
+            <button className ="btn joinBtn" onClick={joinroom}>Join</button>
             <span className="createInfo">
                 If you don't have an invite then create &nbsp;
-                <a href=""  className ="createNewBtn">
+                <a onClick={createNewRoom}href=""  className ="createNewBtn">
                     New Room
                 </a>
             </span>
@@ -32,6 +86,7 @@ function Home() {
 
       </div>
     </div>
+    </>
   )
 }
 
